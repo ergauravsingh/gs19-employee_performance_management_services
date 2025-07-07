f10cd4250277c091861f2c30731b1f186122cde8
Gaurav Singh <gauravsinghlko123@gmail.com>
2025-07-07 10:39:42 +0530

    add details

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # gs19-employee_performance_management_services
 
+@Author: [Gaurav Singh](https://github.com/ergauravsingh)
+
+---
+
 #### Employee Performance Management System
 
 To manage employee performance and provide review comments and score.
@@ -12,10 +16,11 @@ To manage employee performance and provide review comments and score.
 
 
 6) Swagger Docs URL: http://<hostname>:<port>/employee-perf-mgmt/swagger-ui/index.html <br>
-7) Postman Collection: [View Postman Collection]`` <br>
+7) Postman Collection: [Employee Performance Management Services](https://github.com/ergauravsingh/gs19-employee_performance_management_services/blob/master/src/main/resources/postmanCollections/employee_performance_management_services_postmancollection.json) <br>
 8) H2 Console Path: http://<hostname>:<port>/employee-perf-mgmt/h2-console <br>
 9) H2 Connection URL: jdbc:h2:mem:employee-db <br><br>
 
+---
 
 ##### Display Query
 
@@ -29,6 +34,27 @@ WHERE EMPLOYEE.DEPARTMENT_ID = DEPARTMENT.ID
 AND EMPLOYEE.ID = EMPLOYEE_PROJECT.EMPLOYEE_ID 
 AND EMPLOYEE.ID = PERFORMANCE_REVIEW.EMPLOYEE_ID
 
+---
 
 ##### Application Screenshots:
+
+![Swagger Documentation](resources/screenshots/swaggerdocs.JPG)
+*Employee Performance Management System - API Documentation*
+
+---
+
+![Employee Details](resources/screenshots/employeedetails.JPG)
+*Fetch Employee details using ID*
+
+---
+
+![Search by reviewDate](resources/screenshots/searchbyreviewdate.JPG)
+*Get employee details using reviewDate filter*
+
+---
+
+![Search without filters](resources/screenshots/searchwithoutfilters.JPG)
+*Get employee details without using any filter*
+
+---
 
--- src/main/java/com/gaurav/employee/controller/EmployeeController.java
+++ src/main/java/com/gaurav/employee/controller/EmployeeController.java
@@ -15,23 +15,35 @@ import com.gaurav.employee.dto.EmployeeDTO;
 import com.gaurav.employee.dto.EmployeeDetailsDTO;
 import com.gaurav.employee.service.EmployeeService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/employees")
+@Tag(name = "Employee Controller", description = "Manage Employees")
 public class EmployeeController {
 
 	@Autowired
 	private EmployeeService employeeService;
 
+	@Operation(summary = "Get employees with filters", description = "Retrieve a list of employees filtered by review date, departments, and projects")
 	@GetMapping
 	public List<EmployeeDTO> getEmployeesWithFilters(
-			@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate reviewDate,
-			@RequestParam(required = false) List<String> departments,
-			@RequestParam(required = false) List<String> projects) {
+			@Parameter(description = "Review date to filter employees", required = false) @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate reviewDate,
+
+			@Parameter(description = "List of departments to filter employees", required = false) @RequestParam(required = false) List<String> departments,
+
+			@Parameter(description = "List of projects to filter employees", required = false) @RequestParam(required = false) List<String> projects) {
 		return employeeService.getEmployeesWithFilters(reviewDate, departments, projects);
 	}
 
+	@Operation(summary = "Get detailed information of an employee by ID", description = "Retrieve full details of a specific employee using their ID")
 	@GetMapping("/{id}")
-	public EmployeeDetailsDTO getEmployeeDetails(@PathVariable Long id) {
+	public EmployeeDetailsDTO getEmployeeDetails(
+			@Parameter(description = "ID of the employee to retrieve", required = true) @PathVariable Long id) {
 		return employeeService.getEmployeeDetailsById(id);
 	}
 }
